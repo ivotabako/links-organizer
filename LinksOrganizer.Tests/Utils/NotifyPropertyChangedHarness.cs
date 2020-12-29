06d51de50d8412b139939620c5a8a18daed40309
Petar Kanev <pvkanev@gmail.com>
2020-12-29 16:27:32 +0200

    Testing raised property changed

---
+++ LinksOrganizer.Tests/Utils/NotifyPropertyChangedHarness.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace LinksOrganizer.Tests.Utils
+{
+    public class NotifyPropertyChangedHarness
+    {
+        private readonly List<string> changes;
+
+        public List<string> Changes => changes;
+
+        public NotifyPropertyChangedHarness(INotifyPropertyChanged viewModel)
+        {
+            if (viewModel == null)
+            {
+                throw new ArgumentNullException("viewModel", "Argument cannot be null.");
+            }
+
+            changes = new List<string>();
+
+            viewModel.PropertyChanged += new PropertyChangedEventHandler(ViewModel_PropertyChanged);
+        }
+
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            changes.Add(e.PropertyName);
+        }
+    }
+}
